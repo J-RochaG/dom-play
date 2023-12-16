2be843fa982b0aa8cebf61f5c33888482051595e
Jose Rochae <JoseARochaG@outlook.com>
2023-12-16 14:51:53 -0800

    Added the un-clicking feature

--- js/dom-play.js
+++ js/dom-play.js
@@ -2,37 +2,31 @@
 
 //document.querySelector(".hamlet").style.backgroundColor = "orange";
 
-function highlight(el){
-
-    if(el.style.backgroundColor== 'green'){
-        el.style.backgroundColor='white';
-    }else{
-        el.style.backgroundColor='green';
+function highlight(el) {
+    if (el.style.backgroundColor == 'green') {
+        el.style.backgroundColor = 'white';
+    } else {
+        el.style.backgroundColor = 'green';
     }
-
 }
 
-function highlightActor(actor){
-
+function highlightActor(actor) {
     const spans = document.querySelectorAll("div#play span");
-    for(const mySpan of spans){
-        if(mySpan.dataset.actor == actor){
-            mySpan.style.backgroundColor='green';
-        }else{
-            mySpan.style.backgroundColor='white';
+    for (const mySpan of spans) {
+        if (mySpan.dataset.actor == actor) {
+            // Toggle the background color
+            highlight(mySpan);
+        } else {
+            mySpan.style.backgroundColor = 'white';
         }
-    
     }
 }
 
 const spans = document.querySelectorAll("div#play span");
 console.log(spans);
 
-for(const mySpan of spans){
-    mySpan.addEventListener("click", function(ev){
-        //alert(mySpan.dataset.actor);
-
+for (const mySpan of spans) {
+    mySpan.addEventListener("click", function (ev) {
         highlightActor(mySpan.dataset.actor);
     });
-    //alert(mySpan.dataset.actor);
 }
